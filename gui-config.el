d6ff2592cbe393c47182e0765c9736d8c31ad5f5
na4da <34153527+na4da@users.noreply.github.com>
2025-02-27 18:22:43 +0900

    Fix the font size

--- gui-config.el
+++ gui-config.el
@@ -10,7 +10,7 @@
 
 ;; フォント
 (when (>= emacs-major-version 24)
-  (set-face-attribute 'default nil :family "Source Code Pro" :height 96)
+  (set-face-attribute 'default nil :family "Source Code Pro" :height 120)
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0208
                     (font-spec :family "Noto Sans Mono CJK JP")))
